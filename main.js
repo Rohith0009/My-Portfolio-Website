f7ac561301e2ebdc98ae879bed42b9a9e29d498c
Rohith M <87482502+rohithwhitehatjr@users.noreply.github.com>
2022-01-01 17:17:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 console.log("successfull")
 
 
- //Smooth Scrolling Starts
+//Smooth Scrolling Starts
 
- $(document).ready(function () {
- // Add smooth scrolling to all links
+$(document).ready(function () {
+  // Add smooth scrolling to all links
   $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
