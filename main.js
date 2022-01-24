ccb63cc4a33a36ef64f626d5ad181002a378614a
Rohith M <rohithmerala@gmail.com>
2022-01-24 17:20:24 +0530

    V3.3

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-var screen_width = screen.width;
-var screen_height = screen.height;
-
-//Extra Stuff Starts
-
-//Smooth Scrolling Starts
+//Smooth Scrolling
 $(document).ready(function () {
-  $("a").on('click', function (event) {
+  $("a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-        window.location.hash = hash;
-      });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
     }
   });
 });
-//smooth scroling ends
-
-//mobile Nav Close Starts
-
-//mobile Nav Close Ends
-
-//Extra Stuff Ends
+        //***End Of Code***
