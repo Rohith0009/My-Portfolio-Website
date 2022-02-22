c623655b307bcc91e810a6b667607b97b1e296f7
Rohith M <rohithmerala@gmail.com>
2022-02-22 17:09:35 +0530

    V0.0.1

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
 //Smooth Scrolling
-$(document).ready(function () {
-  $("a").on("click", function (event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          window.location.hash = hash;
-        }
-      );
-    }
-  });
-});
+  
         //***End Of Code***
